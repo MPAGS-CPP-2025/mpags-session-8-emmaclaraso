36cb6f3a27d3d641b207080ab5cbac41f91cc391
emmaclaraso <emmcla575@gmail.com>
2025-12-05 11:13:39 +0000

    Exercise  8.1

--- src/MPAGSCipher/ProcessCommandLine.hpp
+++ src/MPAGSCipher/ProcessCommandLine.hpp
@@ -12,18 +12,20 @@
 /**
  * \brief Processes the command-line arguments and modifies accordingly the program settings
  *
- * \param cmdLineArgs The command-line arguments to be processed
- * \param helpRequested Indicates the presence of the help flag in the arguments
- * \param versionRequested Indicates the presence of the version flag in the arguments
- * \param inputFile Name of the input file
- * \param outputFile Name of the output file
- * \param cipherKey Key to be used in encrypting/decrypting routine
- * \param encrypt Flag indicating the mode in which the cipher should run (encrypt = true, decrypt = false)
+ * \param ProgramSettings Struct that holds all the command-line information
  * \return true if the arguments could be successfully parsed, false otherwise
  */
+
+struct ProgramSettings{
+    bool helpRequested{false};
+    bool versionRequested{false};
+    std::string inputFile{}; 
+    std::string outputFile{};
+    std::string cipherKey{};
+    bool encrypt{true};
+}; 
+
 bool processCommandLine(const std::vector<std::string>& cmdLineArgs,
-                        bool& helpRequested, bool& versionRequested,
-                        std::string& inputFile, std::string& outputFile,
-                        std::string& cipherKey, bool& encrypt);
+                        ProgramSettings);
 
 #endif    // MPAGSCIPHER_PROCESSCOMMANDLINE_HPP
